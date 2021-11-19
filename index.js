8e9ec3bd48bb5aed45a67d7b2ed393b1a01fd808
subaru-git <subaru1208.m@gmail.com>
2021-11-19 23:15:55 +0900

    add feed item

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const rss = require('rss-generator');
 const fs = require('fs');
 
-const feed = new rss({title: 'random cat api', description: 'This rss feed make from random cat api.', image_url: 'https://purr.objects-us-east-1.dream.io/i/ODD9FR6.jpg'});
-fs.writeFileSync('feeds/cat.rss', feed.xml());
+const feed = new rss({title: 'random cat api', description: 'This rss feed make from random cat api.'});
+feed.item({title: 'meow', description: 'meow', url: 'https://purr.objects-us-east-1.dream.io/i/ODD9FR6.jpg'})
+fs.writeFileSync('feeds/cat.rss', feed.xml({indent: true}));
